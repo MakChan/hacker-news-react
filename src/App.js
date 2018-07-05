99fc70326b1c3539a69976caebc5cb79628d7c2f
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-07-06 01:31:20 +0530

    Use Re-base for API

--- src/App.js
+++ src/App.js
@@ -1,130 +1,123 @@
 import React, { Component } from "react";
 import "./App.css";
+import Api from "./api";
 
-const list = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
-const PATH_BASE = 'https://hacker-news.firebaseio.com/v0/';
+// const PATH_BASE = 'https://hacker-news.firebaseio.com/v0/';
 // const PATH_SEARCH = '/search';
-const ITEM_ID = '';
-const REQUEST_TYPE = '';
-const DEFAULT_QUERY = '.json?print=pretty';
+// const ITEM_ID = '';
+// const REQUEST_TYPE = '';
+// const DEFAULT_QUERY = '.json?print=pretty';
 
-const item_url = `${PATH_BASE}${REQUEST_TYPE}${ITEM_ID}${DEFAULT_QUERY}`;
+// const item_url = `${PATH_BASE}${REQUEST_TYPE}${ITEM_ID}${DEFAULT_QUERY}`;
 
-const stories_url = `${PATH_BASE}${REQUEST_TYPE}${DEFAULT_QUERY}`;
+// const stories_url = `${PATH_BASE}${REQUEST_TYPE}${DEFAULT_QUERY}`;
+
+
+function fetchSingleStory(id) {
+  // const rank = index + 1;
+  return new Promise(resolve => {
+    Api.fetch(`/item/${id}`, {
+      then(data) {
+        // let item = data;
+        // add the rank since it does not exist yet
+        // item.rank = rank;
+        resolve(data);
+      }
+    });
+  });
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: null,
-      itemStories: [],
-      searchTerm: ""
+      itemStories: []
     };
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.setTopItemStories = this.setTopItemStories.bind(this);  
-    this.findItems = this.findItems.bind(this);    
+    this.fetchTopStories = this.fetchTopStories.bind(this);
   }
 
-  setTopItemStories(result) {
-    this.setState(prevState => ({
-      itemStories: [...prevState.itemStories, result]
-    }))    
+  // setTopItemStories(result) {
+  //   this.setState(prevState => ({
+  //     itemStories: [...prevState.itemStories, result]
+  //   }))    
+  // }
+
+
+  fetchTopStories(storyIds) {
+    let actions = storyIds.slice(0, 30).map(fetchSingleStory);
+    let results = Promise.all(actions);
+    results.then(itemStories =>
+      this.setState({itemStories})       
+    );
   }
 
-  findItems(result) {
-    const request_type = "item/"
 
-    for (let i = 0; i < 20; i++) {
+  // findItems(storiesId) {
+  //   const request_type = "item/"
 
-      fetch(`${PATH_BASE}${request_type}${result[i]}${DEFAULT_QUERY}`)
-      .then(response => response.json())
-      .then(result => this.setTopItemStories(result))
-      .catch(error => error); 
+  //   for (let i = 0; i < 20; i++) {
 
-    }
-  }
+  //     fetch(`${PATH_BASE}${request_type}${storiesId[i]}${DEFAULT_QUERY}`)
+  //     .then(response => response.json())
+  //     .then(result => this.setTopItemStories(result))
+  //     .catch(error => error); 
 
-  onSearchChange(e) {
-    this.setState({ searchTerm: e.target.value });
-  }
+  //   }
+  // }
+
+  // onSearchChange(e) {
+  //   this.setState({ searchTerm: e.target.value });
+  // }
 
   componentDidMount() {
-    const request_type = "topstories"
-    fetch(`${PATH_BASE}${request_type}${DEFAULT_QUERY}`)
-      .then(response => response.json())
-      .then(result => this.findItems(result))
-      .catch(error => error);
+    // const request_type = "topstories"
+    // fetch(`${PATH_BASE}${request_type}${DEFAULT_QUERY}`)
+    //   .then(response => response.json())
+    //   .then(storiesId => this.findItems(storiesId))
+    //   .catch(error => error);
+
+    Api.fetch(`/topstories`, {
+      context: this,
+      then(storyIds) {
+        this.fetchTopStories(storyIds);
+      }
+    });      
+      
   }
     
     
 
   render() {
     console.log(this.state);
-    const { searchTerm, itemStories } = this.state;
-
+    let { itemStories } = this.state;
+    // itemStories = itemStories[0];
+    
     if (!itemStories) { return null; }
 
     return (
       <div className="container">
-      <div>
-        <Search value={searchTerm} onChange={this.onSearchChange}>
-          Search
-        </Search>
-        </div>
-        <Table list={itemStories} pattern={searchTerm}/>
+        <Table list={itemStories}/>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) => (
-  <form>
-    {children} <input type="text" value={value} onChange={onChange} />
-  </form>
-);
-
-const Table = ({ list, pattern }) => (
+const Table = ({ list }) => (
   <div>
-    {list.filter(isSearched(pattern)).map(item => (
-      <div key={item.objectID} className="mt-2">
+    {list.map(item => (
+      <div key={item.id} className="mt-2">
         <span>
           <a href={item.url}>{item.title}</a>
         </span>
         <div>
-          <span>{item.by}</span>
-          <span>{item.descendants}</span>
-          <span>{item.score}</span>
+          <span>{item.by} </span>
+          <span> {item.descendants} </span>
+          <span> {item.score}</span>
         </div>
       </div>
     ))}
   </div>
-);
-
-const Button = ({ onClick, className = "", children }) => (
-  <button onClick={onClick} className={className} type="button">
-    {children}
-  </button>
 );
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,48 @@
+import rebase from "re-base";
+import firebase from "@firebase/app";
+import "@firebase/database";
+
+const HN_DATABASE_URL = "https://hacker-news.firebaseio.com";
+const HN_VERSION = "v0";
+
+const db = firebase.initializeApp({ databaseURL: HN_DATABASE_URL });
+let base = rebase.createClass(db.database());
+
+// Api is a wrapper around base, to include the version child path to the binding automatically.
+const Api = {
+  /**
+   * One way data binding from Firebase to a component's state.
+   * @param {string} endpoint
+   * @param {object} options
+   * @return {object} An object which you can pass to 'removeBinding' if you want to remove the
+   * listener while the component is still mounted.
+   */
+  bindToState(endpoint, options) {
+    return base.bindToState(`/${HN_VERSION}${endpoint}`, options);
+  },
+
+  /**
+   * Listens to a Firebase endpoint without binding changes to a state property. Instead a callback
+   * will be invoked.
+   *
+   * @param {string} endpoint
+   * @param {object} options
+   * @returns
+   */
+  listenTo(endpoint, options) {
+    return base.listenTo(`/${HN_VERSION}${endpoint}`, options);
+  },
+
+  /**
+   * Retrieves data from Firebase once without setting up binding
+   * @param {string} endpoint
+   * @param {object} options
+   * @return {Promise} A Firebase Promise which resolves when the write is complete and rejects
+   * if there is an error.
+   */
+  fetch(endpoint, options) {
+    return base.fetch(`/${HN_VERSION}${endpoint}`, options);
+  }
+};
+
+export default Api;
