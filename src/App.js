e0dfd95e13c17c19c98490b4308d86302c68a754
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-07-06 03:09:58 +0530

    Add Sass Add Bootstrap Add react-timestamp Remove extra code

--- src/App.js
+++ src/App.js
@@ -2,26 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import Api from "./api";
 
-
-// const PATH_BASE = 'https://hacker-news.firebaseio.com/v0/';
-// const PATH_SEARCH = '/search';
-// const ITEM_ID = '';
-// const REQUEST_TYPE = '';
-// const DEFAULT_QUERY = '.json?print=pretty';
-
-// const item_url = `${PATH_BASE}${REQUEST_TYPE}${ITEM_ID}${DEFAULT_QUERY}`;
-
-// const stories_url = `${PATH_BASE}${REQUEST_TYPE}${DEFAULT_QUERY}`;
-
+import Timestamp from "react-timestamp";
 
 function fetchSingleStory(id) {
-  // const rank = index + 1;
   return new Promise(resolve => {
     Api.fetch(`/item/${id}`, {
       then(data) {
-        // let item = data;
-        // add the rank since it does not exist yet
-        // item.rank = rank;
         resolve(data);
       }
     });
@@ -37,67 +23,32 @@ class App extends Component {
     this.fetchTopStories = this.fetchTopStories.bind(this);
   }
 
-  // setTopItemStories(result) {
-  //   this.setState(prevState => ({
-  //     itemStories: [...prevState.itemStories, result]
-  //   }))    
-  // }
-
-
   fetchTopStories(storyIds) {
     let actions = storyIds.slice(0, 30).map(fetchSingleStory);
     let results = Promise.all(actions);
-    results.then(itemStories =>
-      this.setState({itemStories})       
-    );
+    results.then(itemStories => this.setState({ itemStories }));
   }
 
-
-  // findItems(storiesId) {
-  //   const request_type = "item/"
-
-  //   for (let i = 0; i < 20; i++) {
-
-  //     fetch(`${PATH_BASE}${request_type}${storiesId[i]}${DEFAULT_QUERY}`)
-  //     .then(response => response.json())
-  //     .then(result => this.setTopItemStories(result))
-  //     .catch(error => error); 
-
-  //   }
-  // }
-
-  // onSearchChange(e) {
-  //   this.setState({ searchTerm: e.target.value });
-  // }
-
   componentDidMount() {
-    // const request_type = "topstories"
-    // fetch(`${PATH_BASE}${request_type}${DEFAULT_QUERY}`)
-    //   .then(response => response.json())
-    //   .then(storiesId => this.findItems(storiesId))
-    //   .catch(error => error);
-
     Api.fetch(`/topstories`, {
       context: this,
       then(storyIds) {
         this.fetchTopStories(storyIds);
       }
-    });      
-      
+    });
   }
-    
-    
 
   render() {
     console.log(this.state);
     let { itemStories } = this.state;
-    // itemStories = itemStories[0];
-    
-    if (!itemStories) { return null; }
+
+    if (!itemStories) {
+      return null;
+    }
 
     return (
       <div className="container">
-        <Table list={itemStories}/>
+        {itemStories ? <Table list={itemStories} /> : null}
       </div>
     );
   }
@@ -106,14 +57,17 @@ class App extends Component {
 const Table = ({ list }) => (
   <div>
     {list.map(item => (
-      <div key={item.id} className="mt-2">
+      <div key={item.id} className="m-4 p-2 bg-gradient-light">
         <span>
           <a href={item.url}>{item.title}</a>
         </span>
-        <div>
-          <span>{item.by} </span>
-          <span> {item.descendants} </span>
-          <span> {item.score}</span>
+        <div className="small">
+          <span> {item.score} points </span>
+          <span> by {item.by} </span>
+          <span>
+            <Timestamp time={item.time} />  | 
+          </span>
+          <span> {item.descendants} comments</span>
         </div>
       </div>
     ))}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
